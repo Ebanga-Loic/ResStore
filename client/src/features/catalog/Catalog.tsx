102619ea7e56988d5bbef5c65b662bc5bf7a8a76
EBANGA LOIC ARNAUD <ebangaarnaud@gmail.com>
2025-09-10 22:06:36 +0100

    End of Section 8

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,14 +1,42 @@
+import { Grid2, Typography } from '@mui/material';
 import ProductList from './ProductList';
-import { useFetProductsQuery } from './catalogApi';
+import { useFetchFiltersQuery, useFetProductsQuery } from './catalogApi';
+import Filters from './Filters';
+import { useAppDispatch, useAppSelector } from '../../app/store/store';
+import AppPagination from '../../app/shared/components/AppPagination';
+import { setPageNumber } from './catalogSlice';
 
 export default function Catalog() {
-  const { data, isLoading } = useFetProductsQuery();
+  const productParams = useAppSelector((state) => state.catalog);
+  const { data, isLoading } = useFetProductsQuery(productParams);
+  const { data: filtersData, isLoading:filtersLoading } = useFetchFiltersQuery();
+  const dispatch = useAppDispatch();
 
-  if (isLoading || !data) return <h3>Loading...</h3>;
+  if (isLoading || !data || filtersLoading || !filtersData) return <h3>Loading...</h3>;
 
   return (
-    <>
-      <ProductList products={data} />
-    </>
+    <Grid2 container spacing={4}>
+      <Grid2 size={3}>
+        <Filters filtersData={filtersData} />
+      </Grid2>
+      <Grid2 size={9}>
+        {data.items && data.items.length > 0 ? (
+          <>
+            <ProductList products={data.items} />
+            <AppPagination
+              metadata={data.pagination}
+              onPageChange={(page: number) => {
+                dispatch(setPageNumber(page));
+                window.scrollTo({ top: 0, behavior: 'smooth' });
+              }}
+            />
+          </>
+        ) : (
+          <Typography variant="h5">
+            There are no results for this filter
+          </Typography>
+        )}
+      </Grid2>
+    </Grid2>
   );
 }
